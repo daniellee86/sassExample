5ffa79551c115afc8c497126424b952dc4ab2f0b
Daniel Clough <danielleeclough86@hotmail.co.uk>
2022-05-13 14:25:25 +0100

    make initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./css/style.css"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header class="showcase">
+			<div class="container">
+				<nav>
+					<h1 class="logo">My Website</h1>
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Services</a></li>
+					</ul>
+				</nav>
+
+				<div class="showcase-content">
+					<div>
+						<h1>Make Your Marketing Real</h1>
+						<p class="my-1">
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est
+							eligendi tempore atque laborum. Quisquam nemo at non. Corrupti,
+							vitae dolore.
+						</p>
+						<a href="#" class="btn-primary">Learn More</a>
+						<a href="#" class="btn-secondary">Sign Up</a>
+					</div>
+					<img
+						src="https://themesbrand.com/zooki/layout/images/home-2-img.png"
+					/>
+				</div>
+			</div>
+		</header>
+
     </div>
   );
 }
